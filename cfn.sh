d933ddf643f6d7ad8ab52fbd283a98737bb4b72a
UMeGrow Services <91842235+umegrowcloud@users.noreply.github.com>
2022-01-02 12:04:57 +0530

    Update cfn.sh

--- cfn.sh
+++ cfn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aws --debug cloudformation create-stack --stack-name "$serviceName-stack" \
+aws --debug cloudformation update-stack --stack-name "$serviceName-stack" \
     --template-body file://ecs.yaml \
     --region 'us-east-1' \
     --parameters ParameterKey=Stage,ParameterValue=dev \
