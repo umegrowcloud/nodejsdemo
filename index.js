06b427e11645b0e9435a44651ac2d24bef86a474
UMeGrow Services <91842235+umegrowcloud@users.noreply.github.com>
2022-01-02 12:58:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to UMeGrow happy newyear');
+  res.send('Welcome to UMeGrow');
 });
 app.listen(8080, () => {
   console.log('Listening on port 8080');
